58ab8ac4903bc8df7031810d2c994003063367ba
anthonyhaffey <anthony.haffey@gmail.com>
2019-09-30 17:41:16 +0000

    expanded the collector map, and help.html is on the way (but not there)

--- kitten/CollectorMap.js
+++ kitten/CollectorMap.js
@@ -1,8 +1,18 @@
 collector_map = {
-  "default_experiment.json" : "IndexTabs/Simulator/ExperimentEditor/default_experiment.json",
-  
-  /* update the files below" */
-  "ExperimentEditor.html" : "IndexTabs/Simulator/ExperimentEditor/ExperimentEditor.html",
-  
-  "TrialTypeEditor.html" : "IndexTabs/Simulator/TrialTypeEditor/TrialTypeEditor.html"
+
+  //IndexTabs
+    //Help
+      "CondHelp.txt" : "IndexTabs/Help/CondHelp.txt",
+      "GrapHelp.txt" : "IndexTabs/Help/GrapHelp.txt",
+      "MainHelp.txt" : "IndexTabs/Help/MainHelp.txt",
+      "ProcHelp.txt" : "IndexTabs/Help/ProcHelp.txt",
+      "SurvHelp.txt" : "IndexTabs/Help/SurvHelp.txt",
+
+    //Simulator
+      //ExperimentEditor
+        "default_experiment.json" : "IndexTabs/Simulator/ExperimentEditor/default_experiment.json",
+        "ExperimentEditor.html" : "IndexTabs/Simulator/ExperimentEditor/ExperimentEditor.html",
+      //TrialTypeEditor
+        "TrialTypeEditor.html" : "IndexTabs/Simulator/TrialTypeEditor/TrialTypeEditor.html",
+
 }
