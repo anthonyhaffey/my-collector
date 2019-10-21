186197aa6c9152e0b506e043716c5a7c7e873a43
anthonyhaffey <anthony.haffey@gmail.com>
2019-10-21 18:26:55 +0000

    starting to move over from tweetnca to forge for encryption

---
+++ kitten/InitiateKeys.js
@@ -0,0 +1,7 @@
+function initiate_keys(){
+  if(typeof(megaUberJson.keys) == "undefined"){
+    //var user_keys = nacl.box.keyPair();
+
+    //ask the user for a password to encrypt the private/secret key
+  }
+}
