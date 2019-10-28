0cc568e04c1db167dd69eb55bd4816f7a4be3efc
anthonyhaffey <anthony.haffey@gmail.com>
2019-10-28 15:50:31 +0000

    dev_obj now should not overwrite whether the simulator is on or not

--- kitten/Code/initiateCollector.js
+++ kitten/Code/initiateCollector.js
@@ -1,9 +1,10 @@
 //detect if this is local or github or ocollector.org
-dev_obj = {
-  context : "",
-  version : "kitten", //this needs to be updated when moving to cat, but perhaps can just be hardcoded
-  published_links : []
+if(typeof(dev_obj) == "undefined"){
+  dev_obj = {};
 }
+dev_obj.context = "";
+dev_obj.version = "kitten"; //this needs to be updated when moving to cat, but perhaps can just be hardcoded
+dev_obj.published_links = [];
 function detect_context(){
   if(document.URL.indexOf("localhost") !== -1){
     return "localhost";
@@ -27,7 +28,7 @@ function initiate_collector(){
       console.dir("hi");
       $.post("code/initiateCollector.php",{
 				//nothing to post, just want to run it.
-      },function(local_key){				
+      },function(local_key){
 				$("#login_div").show();
         if(local_key !== "no-key"){
           window.localStorage.setItem("local_key",local_key);
