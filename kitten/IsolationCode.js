955867fbeb597ecbfba306088ec79cfed8919b99
anthonyhaffey <anthony.haffey@gmail.com>
2019-12-04 14:26:46 +0000

    migrating some code to "IsolationCode.js" to try to make is easier to edit (and preview) files in isolation

---
+++ kitten/IsolationCode.js
@@ -0,0 +1,18 @@
+///////////////
+// Libraries //
+///////////////
+
+// Collector specific
+
+window.collector_map || document.write('<script src="https://anthonyhaffey.github.io/my-collector/kitten/CollectorMap.js"><\/script>');
+
+window.Handsontable || document.write('<link rel="stylesheet" href="https://anthonyhaffey.github.io/my-collector/kitten/IndexTabs/Simulator/handsontables/handsontables.full.css">' +
+'<script type="text/javascript" charset="utf-8" src="https://anthonyhaffey.github.io/my-collector/kitten/IndexTabs/Simulator/handsontables/handsontables.full.js">' + 
+'<\/script><script type="text/javascript" charset="utf-8" src="https://anthonyhaffey.github.io/my-collector/kitten/IndexTabs/Simulator/HandsontableFunctions.js"><\/script>');
+
+//Publicly available libraries
+window.jQuery        || document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"><\/script>');
+
+window.bootstrap || document.write('<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"><\/script><link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">');
+
+window.Papa          || document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/PapaParse/5.1.0/papaparse.min.js"><\/script>');
